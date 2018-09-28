b9e60f0206e3342e6d50308fa27af744a065e0c4
soft_tangxiaoxian <soft_tangxiaoxian@163.com>
2018-09-28 17:53:21 +0800

    body parser

--- app.mjs
+++ app.mjs
@@ -1,11 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 
 import indexAPI from './src/router/index';
 import imageAPI from './src/router/image';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.use("/api", indexAPI);
