183df123f7f7265959bb5bb489ca0075ca853a87
soft_tangxiaoxian <soft_tangxiaoxian@163.com>
2018-09-29 18:12:12 +0800

    tesseract process

--- src/router/image.mjs
+++ src/router/image.mjs
@@ -9,6 +9,11 @@ import {res2ok, res2msg} from '../common/result';
 const router = express.Router();
 
 async function done(filename, path, res) {
+    if (!path) {
+        res.json(res2msg('path is null.path=' + path, 500));
+        return;
+    }
+
     let newpath = path.substring(0, path.lastIndexOf('/') + 1) + filename;
     let status = await rename(path, newpath);
     if (status) {
@@ -49,7 +54,7 @@ router.post('/orc', (req, res) => {
             res.json(res2ok({'result':'没有上传图片!'}));
             return;
         }
-
+    
         let path = files[0][1].path;
         let filename = files[0][0];
         done(filename, path, res);
